f77add55d6537b9104a9bf3f2d527b416a1f337f
Andreick <andreickgomes00@gmail.com>
2020-10-30 20:08:15 -0300

    Melhorias na bateria de testes

--- aps.c
+++ aps.c
@@ -2,16 +2,24 @@
 #include <stdlib.h>
 #include <time.h>
 
-// Nome do arquivo binário que contém os números
-#define ARQUIVO "vetor_100000.txt"
+// Nome do arquivo binario que contem um vetor desordenado com um milhao de posicoes
+#define ARQ_VET_DESORD "vetor_1000000_desordenado.txt"
 
-// Quantidade de números que serão ordenados
+// Nome do arquivo texto que contem os resultados das ordenacoes
+#define ARQ_SAIDA "saida.txt"
+
+#define ARQ_VET_ORD "vetor_ordenado.txt"
+
+#define ARQ_VET_PCTO "vetor_percentualmente_ordenado.txt"
+
+// Quantidade de numeros que serao ordenados
 enum {QNT_IMAGENS = 100000};
 
+// Indicadores de qual parte do vetor ordenar
 enum {INICIO, MEIO, FIM};
 
 void insertionSort(int arr[], int n) {
-    int i, key, j;
+    int i, j, key;
 
     for (i = 1; i < n; i++) {
 
@@ -20,7 +28,7 @@ void insertionSort(int arr[], int n) {
 
         while (j >= 0 && arr[j] > key) {
             arr[j + 1] = arr[j];
-            j = j - 1;
+            j--;
         }
 
         arr[j + 1] = key;
@@ -51,94 +59,148 @@ void selectionSort(int arr[], int n) {
 void quickSort(int arr[], int first, int last) {
     int i, j, pivot, aux;
     i = first;
-    j = last;
-    pivot = arr[first];
+    j = last - 1;
+    pivot = arr[(first + last) / 2];
 
     while(i <= j) {
 
-        while(arr[i] < pivot && i < last)
+        while(arr[i] < pivot)
             i++;
 
-        while(arr[j] > pivot && j > first)
+        while(arr[j] > pivot)
             j--;
 
         if(i <= j) {
-            aux = arr[i];
-            arr[i] = arr[j];
-            arr[j] = aux;
+
+            if (arr[i] != arr[j]) {
+                aux = arr[i];
+                arr[i] = arr[j];
+                arr[j] = aux;
+            }
+
             i++;
             j--;
         }
     }
 
     if(j > first)
-        quickSort(arr, first, j);
+        quickSort(arr, first, j + 1);
     
     if(i < last)
         quickSort(arr, i, last);
 }
 
-/* Preenche o vetor com os valores contidos no arquivo
-que será retornado pela função
-Cria o arquivo se ele não existir */
-FILE* iniciaVetor(int vet[]) {
+/* Preenche o vetor com os valores contidos no arquivo do vetor desordenado
+Cria o arquivo se ele nao existir */
+void iniciaVetorDesordenado(int vet[]) {
     FILE *arq;
     int i;
 
-    arq = fopen(ARQUIVO, "rb");
+    arq = fopen(ARQ_VET_DESORD, "rb");
 
     if (arq == NULL) {
+        int num;
         
-        arq = fopen(ARQUIVO, "wb");
+        arq = fopen(ARQ_VET_DESORD, "wb");
 
         srand(time(NULL));
 
-        for (i = 0; i < QNT_IMAGENS; i++) {
+        for (i = 0; i < 1000000; i++) {
 
-            // Gera um número inteiro aleatório na faixa de -500000 a 499999
-            vet[i] = 1000 * (rand() % 1000) + (rand() % 1000) - 500000;
+            // Gera um numero inteiro aleatorio na faixa de -1000000 a 999999
+            num = 1000 * (rand() % 2000) + (rand() % 1000) - 1000000;
+            fwrite(&num, sizeof(int), 1, arq);
         }
-
-        fwrite(vet, sizeof(int), QNT_IMAGENS, arq);
 
         fclose(arq);
 
-        arq = fopen(ARQUIVO, "rb");
-
-        return arq;
+        arq = fopen(ARQ_VET_DESORD, "rb");
     }
 
     i = fread(vet, sizeof(int), QNT_IMAGENS, arq);
     if (i != QNT_IMAGENS) {
-        printf("Erro na leitura do arquivo "ARQUIVO"!\n");
+        printf("Erro na leitura do arquivo "ARQ_VET_DESORD"!\n\n");
+        printf("Pressione a tecla Enter para continuar. . . ");
         getchar();
         exit(1);
     }
 
-    return arq;
+    fclose(arq);
 }
 
-/* Retorna para o começo do arquivo e preenche o vetor
-novamente com os valores iniciais */
-void reiniciaVetor(int vet[], FILE *arq) {
-	rewind(arq);
-	fread(vet, sizeof(int), QNT_IMAGENS, arq);
+void iniciaVetorOrdenado(int vet[]) {
+    FILE *arq;
+
+    quickSort(vet, 0, QNT_IMAGENS);
+
+    arq = fopen(ARQ_VET_ORD, "wb");
+
+    fwrite(vet, sizeof(int), QNT_IMAGENS, arq);
+
+    fclose(arq);
 }
 
+/* Preenche o vetor com os valores contidos no arquivo */
+void sobrescreverVetor(int vet[], char nome_arq[]) {
+	FILE *arq;
+
+    arq = fopen(nome_arq, "rb");
+
+    fread(vet, sizeof(int), QNT_IMAGENS, arq);
+
+    fclose(arq);
+}
+
+void embaralhaVetor(int vet[], int primeiro, int ultimo) {
+    int i, j, aux;
+    int faixa = (ultimo - primeiro) / 100;
+
+    srand(1);
+
+    for (i = primeiro; i < ultimo; i++) {
+        
+        j = 100 * (rand() % faixa) + (rand() % 100) + primeiro;
+
+        if (vet[i] != vet[j]) {
+            aux = vet[i];
+            vet[i] = vet[j];
+            vet[j] = aux;
+        }
+    }
+}
+
+/* Ordena uma porcentagem de um vetor desordenado
+A ordenacao pode ser feita com os elementos do inicio, meio, ou final do vetor */
 void ordenaPercentualmente(int vet[], float porcentagem, int posicao) {
-    int tamanho = porcentagem * QNT_IMAGENS;
+    FILE *arq;
+    int tamanho = (1 - porcentagem) * QNT_IMAGENS;
 
     switch (posicao) {
         case INICIO:
-            quickSort(vet, 0, tamanho - 1);
+            sobrescreverVetor(vet, ARQ_VET_ORD);
+
+            embaralhaVetor(vet, QNT_IMAGENS - tamanho, QNT_IMAGENS);
             break;
         case MEIO:
-            quickSort(vet, (QNT_IMAGENS / 2) - (tamanho / 2), (QNT_IMAGENS / 2) + (tamanho / 2) - 1);
+            sobrescreverVetor(vet, ARQ_VET_ORD);
+
+            embaralhaVetor(vet, 0, (tamanho / 2));
+            embaralhaVetor(vet, QNT_IMAGENS - (tamanho / 2), QNT_IMAGENS);
             break;
         case FIM:
-            quickSort(vet, QNT_IMAGENS - tamanho, QNT_IMAGENS - 1);
+            sobrescreverVetor(vet, ARQ_VET_ORD);
+
+            embaralhaVetor(vet, 0, tamanho);
             break;
+        default:
+            sobrescreverVetor(vet, ARQ_VET_DESORD);
     }
+
+    arq = fopen(ARQ_VET_PCTO, "wb");
+
+    fwrite(vet, sizeof(int), QNT_IMAGENS, arq);
+
+    fclose(arq);
 }
 
 void imprimeVetor(int vet[]) {
@@ -156,24 +218,50 @@ void imprimeVetor(int vet[]) {
 }
 
 int main() {
-	FILE *arquivo;
+    FILE *arq_saida;
 	int vetor[QNT_IMAGENS];
     clock_t tempo;
     double tempo_total;
     float porcentagem;
     int posicao;
 
-    arquivo = iniciaVetor(vetor);
+    iniciaVetorDesordenado(vetor);
+    iniciaVetorOrdenado(vetor);
+
+    arq_saida = fopen(ARQ_SAIDA, "a");
+    printf("Comparacao de metodos de ordenacao com um vetor de %d posicoes\n\n", QNT_IMAGENS);
+    fprintf(arq_saida, "\tComparaÃ§Ã£o de mÃ©todos de ordenaÃ§Ã£o com um vetor de %d posiÃ§Ãµes\n\n\n", QNT_IMAGENS);
 
     for (porcentagem = 0; porcentagem < 1; porcentagem += 0.25) {
         
         for (posicao = INICIO; posicao <= FIM; posicao++) {
 
-            ordenaPercentualmente(vetor, porcentagem, posicao);
-            //imprimeVetor(vetor);
-            printf("Vetor %.f%% ordenado no %d\n", porcentagem * 100, posicao);
+            if (porcentagem == 0)
+                posicao = FIM + 1;
+
+            switch (posicao) {
+                case INICIO:
+                    printf("  Vetor %.f%% ordenado no inicio", porcentagem * 100);
+                    fprintf(arq_saida, "Vetor %.f%% ordenado no inÃ­cio", porcentagem * 100);
+                    break;
+                case MEIO:
+                    printf("  Vetor %.f%% ordenado no meio", porcentagem * 100);
+                    fprintf(arq_saida, "Vetor %.f%% ordenado no meio", porcentagem * 100);
+                    break;
+                case FIM:
+                    printf("  Vetor %.f%% ordenado no fim", porcentagem * 100);
+                    fprintf(arq_saida, "Vetor %.f%% ordenado no fim", porcentagem * 100);
+                    break;
+                default:
+                    printf("  Vetor desordenado");
+                    fputs("Vetor desordenado", arq_saida);
+            }
 
             //=== Insertionsort
+
+            ordenaPercentualmente(vetor, porcentagem, posicao);
+
+            printf("\n\n- Insertionsort\n\nOrdenando. . . ");
 
             tempo = clock(); // Tempo inicial
             insertionSort(vetor, QNT_IMAGENS);
@@ -181,13 +269,14 @@ int main() {
 
             tempo_total = ((double) tempo) / CLOCKS_PER_SEC; // Tempo em segundos
 
-            printf("\nInsertionsort\nTempo total: %.3lf segundos\n", tempo_total);
-            //imprimeVetor(vetor);
+            printf("\n\nTempo total: %lf segundos\n", tempo_total);
+            fprintf(arq_saida, "\n\n\tInsertionsort\n\n\t\tTempo total: %lf segundos\n", tempo_total);
 
             //=== Selectionsort
 
-            reiniciaVetor(vetor, arquivo);
-            ordenaPercentualmente(vetor, porcentagem, posicao);
+            sobrescreverVetor(vetor, ARQ_VET_PCTO);
+
+            printf("\n- Selectionsort\n\nOrdenando. . . ");
 
             tempo = clock(); // Tempo inicial
             selectionSort(vetor, QNT_IMAGENS);
@@ -195,33 +284,30 @@ int main() {
 
             tempo_total = ((double) tempo) / CLOCKS_PER_SEC; // Tempo em segundos
 
-            printf("\nSelectionsort\nTempo total: %.3lf segundos\n", tempo_total);
-            //imprimeVetor(vetor);
-
+            printf("\n\nTempo total: %lf segundos\n", tempo_total);
+            fprintf(arq_saida, "\n\tSelectionsort\n\n\t\tTempo total: %lf segundos\n", tempo_total);
+            
             //=== QuickSort
 
-            reiniciaVetor(vetor, arquivo);
-            ordenaPercentualmente(vetor, porcentagem, posicao);
+            sobrescreverVetor(vetor, ARQ_VET_PCTO);
+
+            printf("\n- Quicksort\n\nOrdenando. . . ");
 
             tempo = clock(); // Tempo inicial
-            quickSort(vetor, 0, QNT_IMAGENS - 1);
+            quickSort(vetor, 0, QNT_IMAGENS);
             tempo = clock() - tempo; // Tempo final - inicial
 
             tempo_total = ((double) tempo) / CLOCKS_PER_SEC; // Tempo em segundos
 
-            printf("\nQuicksort\nTempo total: %.3lf segundos\n", tempo_total);
-            //imprimeVetor(vetor);
+            printf("\n\nTempo total: %lf segundos\n", tempo_total);
+            fprintf(arq_saida, "\n\tQuicksort\n\n\t\tTempo total: %lf segundos\n", tempo_total);
 
-            printf("\n===============================\n\n");
-
-            reiniciaVetor(vetor, arquivo);
-            
-            if (porcentagem == 0)
-                break;
+            printf("\n================================\n\n");
+            fputs("\n===============================================\n\n", arq_saida);
         }
     }
-
-	fclose(arquivo);
+    
+    fclose(arq_saida);
 
 	printf("Pressione a tecla Enter para continuar. . . ");
 	getchar();
